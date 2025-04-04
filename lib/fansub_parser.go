f2141e51d2793ab1c6a98af629580c7156d29079
Jaeiya <exanix@gmail.com>
2025-04-04 13:17:59 -0700

    feat(fansub_parser): support `TV Asahi` source

--- lib/fansub_parser.go
+++ lib/fansub_parser.go
@@ -56,6 +56,7 @@ var fansubSourceMap = map[string]string{
 	"dsnp":     "Disney+",
 	"cr":       "CrunchyRoll",
 	"hidive":   "HIDIVE",
+	"cx":       "TV Asahi",
 
 	"web-dl": "WebRip",
 	"webrip": "WebRip",
