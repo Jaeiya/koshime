818d07139fe194d93a730c63092a42dc60a9ac43
Jaeiya <exanix@gmail.com>
2025-04-04 13:54:43 -0700

    feat(fansub_parser): add `YouTube` source

--- lib/fansub_parser.go
+++ lib/fansub_parser.go
@@ -56,6 +56,7 @@ var fansubSourceMap = map[string]string{
 	"dsnp":     "Disney+",
 	"cr":       "CrunchyRoll",
 	"hidive":   "HIDIVE",
+	"yt":       "YouTube",
 	"cx":       "TV Asahi",
 
 	"bs8":  "TV Tokyo",
