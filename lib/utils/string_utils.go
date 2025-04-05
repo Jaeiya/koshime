2d65f73bddab18d5e4fce72450919bfd362c1187
Jaeiya <exanix@gmail.com>
2025-04-05 09:17:50 -0700

    fix(string_utils): do not parse empty string

--- lib/utils/string_utils.go
+++ lib/utils/string_utils.go
@@ -28,6 +28,10 @@ func ReplaceAll(s string, tokenMap map[string]string) string {
 }
 
 func RemoveBrackets(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
 	if s[0] == '[' || s[0] == '(' {
 		s = s[1:]
 	}
