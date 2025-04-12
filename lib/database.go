9c39bd056df57fa69c031ffb7f35a8521b79db10
Jaeiya <exanix@gmail.com>
2025-04-12 14:21:55 -0700

    feat: add database handler

---
+++ lib/database.go
@@ -0,0 +1,88 @@
+package lib
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/jaeiya/koshime/lib/utils"
+	"github.com/shamaton/msgpack/v2"
+)
+
+var dbFileName = "koshime.db"
+
+type DBProfile struct {
+	ID string
+	// Profile & library
+	links           []string
+	SecondsWatched  int
+	CompletedSeries int
+	Username        string
+	AccessToken     string
+	RefreshToken    string
+	TokenExpiration int
+}
+
+type DBLibEntry struct {
+	ID        string
+	JPN_Title string
+	ENG_Title string
+	Synonyms  []string
+	Episodes  int
+	Progress  int
+	Slug      string
+}
+
+type DBConfig struct {
+	UseColors bool
+}
+
+type DBData struct {
+	Config  DBConfig
+	Profile DBProfile
+	Library []DBLibEntry
+}
+
+type Database struct {
+	isLoaded bool
+	data     DBData
+}
+
+func (Database) New(data DBData) (*Database, error) {
+	db := &Database{false, data}
+	db.isLoaded = true
+	err := db.Save()
+	if err != nil {
+		return &Database{}, err
+	}
+	return db, nil
+}
+
+func (db *Database) Load() error {
+	if db.isLoaded {
+		return nil
+	}
+	file, err := os.ReadFile(filepath.Join(utils.GetWorkingDir(), dbFileName))
+	if err != nil {
+		return err
+	}
+	msgpack.Unmarshal(file, &db.data)
+	db.isLoaded = true
+	return nil
+}
+
+func (db Database) Save() error {
+	bytes, err := msgpack.Marshal(db.data)
+	if err != nil {
+		return err
+	}
+	os.WriteFile(dbFileName, bytes, 0o644)
+	return nil
+}
+
+func (db Database) GetData() (*DBData, error) {
+	if !db.isLoaded {
+		return &DBData{}, fmt.Errorf("database has not been loaded")
+	}
+	return &db.data, nil
+}
--- lib/utils/basic_utils.go
+++ lib/utils/basic_utils.go
@@ -1,8 +1,22 @@
 package utils
 
 import (
+	"os"
 	"strconv"
 )
+
+// GetWorkingDir gets the working directory without returning
+// an error.
+//
+// 🔴 Will panic if an error occurs. This function should
+// never error, so we panic on unexpected behavior.
+func GetWorkingDir() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return wd
+}
 
 func IsNumber(s string) bool {
 	_, err := strconv.Atoi(s)
