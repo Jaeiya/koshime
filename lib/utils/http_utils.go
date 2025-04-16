b439cb3da5ee4d14473a7291f2497622d0412fd1
Jaeiya <exanix@gmail.com>
2025-04-16 16:18:27 -0700

    feat(utils): add `PostJSON` helper func
    This might be integrated directly into the kitsu package, but we'll see.

---
+++ lib/utils/http_utils.go
@@ -0,0 +1,62 @@
+package utils
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+const userAgent = "Koshime/0.1"
+
+var client = &http.Client{}
+
+func PostJSON[T any](url string, content []byte, data *T) error {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(content))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", userAgent)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Accept-Encoding", "gzip")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	var reader io.ReadCloser
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		reader, err = gzip.NewReader(resp.Body)
+		if err != nil {
+			return err
+		}
+	} else {
+		reader = resp.Body
+	}
+	defer reader.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		body, _ := io.ReadAll(reader)
+		bodyStr := string(body)
+		if resp.StatusCode == 400 {
+			if strings.Contains(bodyStr, "invalid_grant") {
+				return fmt.Errorf("invalid username or password")
+			}
+		}
+		return fmt.Errorf("HTTP %d: %s", resp.StatusCode, bodyStr)
+	}
+
+	err = json.NewDecoder(reader).Decode(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
