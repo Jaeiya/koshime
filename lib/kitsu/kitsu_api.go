8d1da8cae0c3c8a2b2543bdfeee864fe7edabfe0
Jaeiya <exanix@gmail.com>
2025-04-17 08:38:05 -0700

    feat(kitsu_api): move `postJSON()` to kitsu api
    This makes more sense, since these methods are going to be specific to
    the Kitsu API, not general-purpose.

--- lib/kitsu/kitsu_api.go
+++ lib/kitsu/kitsu_api.go
@@ -1,21 +1,31 @@
 package kitsu
 
 import (
+	"bytes"
+	"compress/gzip"
 	"encoding/json"
-
-	"github.com/jaeiya/koshime/lib/utils"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
 )
 
-type TokenData struct {
-	Token        string `json:"access_token"`
-	TokenType    string `json:"token_type"`
+const clientAgent = "Koshime/0.1"
+
+var client = &http.Client{}
+
+type AuthToken struct {
+	Token     string `json:"access_token"`
+	TokenType string `json:"token_type"`
+	// Seconds until token expires
 	ExpiresIn    int    `json:"expires_in"`
 	RefreshToken string `json:"refresh_token"`
 	Scope        string `json:"scope"`
-	CreatedAt    int    `json:"created_at"`
+	// Seconds since unix epoch
+	CreatedAt int `json:"created_at"`
 }
 
-func GetAuthToken(userName, password string) (TokenData, error) {
+func GetAuthToken(userName, password string) (AuthToken, error) {
 	credentials := map[string]string{
 		"grant_type": "password",
 		"username":   userName,
@@ -24,14 +34,106 @@ func GetAuthToken(userName, password string) (TokenData, error) {
 
 	payload, err := json.Marshal(credentials)
 	if err != nil {
-		return TokenData{}, err
+		return AuthToken{}, err
 	}
 
-	var data TokenData
-	err = utils.PostJSON(apiAuthTokenURL, payload, &data)
+	var data AuthToken
+	err = postJSON(apiAuthTokenURL, payload, "", &data)
 	if err != nil {
 		panic(err)
 	}
 
+	return data, nil
+}
+
+func postJSON[T any](url string, payload []byte, bearerToken string, data *T) error {
+	req, err := NewJsonRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		return err
+	}
+
+	if bearerToken != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", bearerToken))
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	body, err := readResponseBody(resp)
+	if err != nil {
+		return err
+	}
+	bodyStr := string(body)
+
+	if resp.StatusCode >= 300 && resp.StatusCode < 400 {
+		return fmt.Errorf("HTTP %d: %s", resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+
+	if resp.StatusCode >= 400 && resp.StatusCode < 500 {
+		if strings.Contains(bodyStr, "invalid_grant") {
+			return fmt.Errorf("invalid username or password")
+		}
+		return fmt.Errorf(
+			"ClientError::HTTP %d: %s\n%s",
+			resp.StatusCode,
+			http.StatusText(resp.StatusCode),
+			bodyStr,
+		)
+	}
+
+	if resp.StatusCode >= 500 {
+		return fmt.Errorf(
+			"ServerError::HTTP %d: %s",
+			resp.StatusCode,
+			http.StatusText(resp.StatusCode),
+		)
+	}
+
+	err = json.NewDecoder(bytes.NewReader(body)).Decode(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewJsonRequest(method, url string, body *bytes.Buffer) (*http.Request, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", clientAgent)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Accept-Encoding", "gzip")
+
+	return req, nil
+}
+
+func readResponseBody(resp *http.Response) ([]byte, error) {
+	var reader io.ReadCloser
+	var err error
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		reader, err = gzip.NewReader(resp.Body)
+		if err != nil {
+			resp.Body.Close()
+			return []byte{}, err
+		}
+		defer func() {
+			reader.Close()
+			resp.Body.Close()
+		}()
+	} else {
+		reader = resp.Body
+		defer reader.Close()
+	}
+
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return []byte{}, err
+	}
 	return data, nil
 }
