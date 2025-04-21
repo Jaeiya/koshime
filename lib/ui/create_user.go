44acbc1c2dfcfdf17991ee4269b8abd00de4e5b5
Jaeiya <exanix@gmail.com>
2025-04-21 14:46:50 -0700

    fix(create_user): do not use `q` for quit
    This interferes with field input and esc key makes more sense in most
    cases.

--- lib/ui/create_user.go
+++ lib/ui/create_user.go
@@ -60,7 +60,7 @@ var keys = keyMap{
 	Up:    key.NewBinding(key.WithKeys("up", "k"), key.WithHelp("↑/k", "move up")),
 	Down:  key.NewBinding(key.WithKeys("down", "j"), key.WithHelp("↓/j", "move down")),
 	Enter: key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "select")),
-	Quit:  key.NewBinding(key.WithKeys("q", "esc", "ctrl+c"), key.WithHelp("q", "quit")),
+	Quit:  key.NewBinding(key.WithKeys("esc", "ctrl+c"), key.WithHelp("esc", "quit")),
 	Help:  key.NewBinding(key.WithKeys("shift+/"), key.WithHelp("?", "toggle help")),
 }
 
