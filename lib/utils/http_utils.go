35d025317d70f78236e262aa45f311b9efcb2c3c
Jaeiya <exanix@gmail.com>
2025-04-29 07:51:24 -0700

    feat: add http utils
    This will replace a lot of existing http code

---
+++ lib/utils/http_utils.go
@@ -0,0 +1,84 @@
+package utils
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+)
+
+const userAgent = "Koshime/0.1"
+
+var client http.Client
+
+type HttpResponse struct {
+	StatusCode int
+	StatusText string
+	Body       []byte
+}
+
+type Http struct{}
+
+func (Http) GetUserAgent() string {
+	return userAgent
+}
+
+// Do executes an http request with the specified Accept
+// and Content-Type headers, which can be left empty.
+//
+// 🟡 If accept is empty, it will use text/plain
+//
+// 🟡 If contentType is empty, it will not be used
+func (h Http) Do(req *http.Request, accept, contentType string) (HttpResponse, error) {
+	if contentType != "" {
+		req.Header.Set("Content-Type", contentType)
+	}
+
+	if accept == "" {
+		accept = "text/plain"
+	}
+
+	req.Header.Set("Accept", accept)
+	req.Header.Set("User-Agent", userAgent)
+	req.Header.Set("Accept-Encoding", "gzip")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return HttpResponse{}, err
+	}
+
+	body, err := h.ReadResponseBody(resp)
+	if err != nil {
+		return HttpResponse{}, err
+	}
+
+	return HttpResponse{
+		StatusCode: resp.StatusCode,
+		StatusText: http.StatusText(resp.StatusCode),
+		Body:       body,
+	}, nil
+}
+
+func (Http) ReadResponseBody(resp *http.Response) ([]byte, error) {
+	var reader io.ReadCloser
+	var err error
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		reader, err = gzip.NewReader(resp.Body)
+		if err != nil {
+			resp.Body.Close()
+			return []byte{}, err
+		}
+		defer func() {
+			reader.Close()
+			resp.Body.Close()
+		}()
+	} else {
+		reader = resp.Body
+		defer reader.Close()
+	}
+
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return []byte{}, err
+	}
+	return data, nil
+}
