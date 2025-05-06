c58f5ec57550edaf5af391b2215e2bbefc64b6af
Jaeiya <exanix@gmail.com>
2025-05-06 16:34:52 -0700

    feat(main): update to latest changes

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"path/filepath"
 
-	"github.com/Jaeiya/koshime/lib/database"
-	"github.com/Jaeiya/koshime/lib/ui"
-	"github.com/Jaeiya/koshime/lib/utils"
+	"github.com/Jaeiya/koshime/lib/views"
+	tea "github.com/charmbracelet/bubbletea/v2"
 )
 
 var (
@@ -16,39 +14,15 @@ var (
 )
 
 func main() {
-	fmt.Println("")
-	db, err := InitDatabase(dbFilePath)
+	m, err := views.New(dbFilePath)
 	if err != nil {
-		if errors.Is(err, errUserAborted) {
-			return
-		}
 		panic(err)
 	}
 
-	fmt.Printf("%+v", db)
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		panic(err)
+	}
 
 	// 	fmt.Println("    Date:", entry.Date.Format("01/02/2006 3:04 PM"))
-}
-
-func InitDatabase(path string) (*database.Database, error) {
-	var db *database.Database
-	var err error
-	if !utils.FileExists(path) {
-		data, isAborted := ui.NewUser()
-		if isAborted {
-			return db, errUserAborted
-		}
-		db, err = database.NewDatabase(&data)
-		if err != nil {
-			return db, err
-		}
-		return db, nil
-	}
-
-	db, err = database.NewDatabase(nil)
-	err = db.Load()
-	if err != nil {
-		return db, err
-	}
-	return db, nil
 }
