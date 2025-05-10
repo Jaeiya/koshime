b5a48fde05fb4c0fa235c32f67f2b2055af4345e
Jaeiya <exanix@gmail.com>
2025-05-10 06:32:13 -0700

    refactor(style): move list and textinput into `component` file

--- lib/ui/styles.go
+++ lib/ui/styles.go
@@ -1,9 +1,6 @@
 package ui
 
 import (
-	"github.com/charmbracelet/bubbles/v2/key"
-	"github.com/charmbracelet/bubbles/v2/list"
-	"github.com/charmbracelet/bubbles/v2/textinput"
 	"github.com/charmbracelet/lipgloss/v2"
 	"github.com/charmbracelet/x/ansi"
 )
@@ -23,63 +20,3 @@ var (
 	inputPromptStyle = Style.Foreground(ansi.BrightGreen)
 	inputTextStyle   = Style.Foreground(ansi.BrightWhite)
 )
-
-func NewTextInput() textinput.Model {
-	input := textinput.New()
-	input.SetWidth(20)
-	input.Focus()
-	input.Prompt = "   > "
-	input.EchoCharacter = '•'
-	input.Styles.Focused.Prompt = inputPromptStyle
-	input.Styles.Focused.Text = inputTextStyle
-	return input
-}
-
-type ListOptions struct {
-	Items         []list.Item
-	ShortHelpKeys []key.Binding
-	Width         int
-	MaxHeight     int
-	ItemsPerPage  int
-}
-
-func NewList(o ListOptions) list.Model {
-	d := list.NewDefaultDelegate()
-	d.Styles.SelectedTitle = d.Styles.SelectedTitle.Foreground(ansi.BrightGreen).
-		BorderForeground(ansi.BrightGreen)
-	d.Styles.SelectedDesc = d.Styles.SelectedTitle.Foreground(ansi.Blue)
-	d.Styles.NormalTitle = d.Styles.NormalTitle.Foreground(ansi.White)
-	d.Styles.NormalDesc = d.Styles.NormalDesc.Foreground(ansi.BrightBlack)
-	d.Styles.FilterMatch = Style
-
-	itemsPerPage := min(len(o.Items), o.ItemsPerPage)
-	itemHeight := itemsPerPage * 3 // items plus padding
-	offset := 6                    // size of non-items
-	if len(o.Items) < itemsPerPage {
-		offset = 0
-	}
-	height := min(o.MaxHeight, itemHeight+offset)
-
-	l := list.New(o.Items, d, o.Width, height)
-	l.SetShowTitle(false)
-	l.DisableQuitKeybindings()
-	l.FilterInput.VirtualCursor = false
-
-	l.Help.Styles.ShortDesc = HelpDescStyle
-	l.Help.Styles.FullDesc = HelpDescStyle
-	l.Help.Styles.ShortKey = HelpKeyStyle
-	l.Help.Styles.FullKey = HelpKeyStyle
-	l.Styles.StatusBar = l.Styles.StatusBar.MarginLeft(3).
-		Foreground(HelpKeyStyle.GetForeground())
-	l.Styles.StatusBarFilterCount = HelpDescStyle
-	l.FilterInput.Styles.Focused.Prompt = l.Styles.Filter.Focused.Prompt.Foreground(ansi.Yellow)
-
-	if o.ShortHelpKeys != nil {
-		l.AdditionalShortHelpKeys = func() []key.Binding {
-			return o.ShortHelpKeys
-		}
-		l.AdditionalFullHelpKeys = l.AdditionalShortHelpKeys
-	}
-
-	return l
-}
