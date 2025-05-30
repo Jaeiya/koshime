81d6edfe930982cc2c1bfc2c86e6d79cb06d12ac
Jaeiya <exanix@gmail.com>
2025-05-30 14:05:52 -0700

    feat: add menu view
    This separates the menu from the main view model

---
+++ lib/views/menu.go
@@ -0,0 +1,194 @@
+package views
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/Jaeiya/koshime/lib/database"
+	"github.com/Jaeiya/koshime/lib/ui"
+	"github.com/Jaeiya/koshime/lib/utils"
+	"github.com/charmbracelet/bubbles/v2/help"
+	"github.com/charmbracelet/bubbles/v2/key"
+	tea "github.com/charmbracelet/bubbletea/v2"
+	"github.com/charmbracelet/lipgloss/v2"
+	"github.com/charmbracelet/x/ansi"
+)
+
+type ExitToMenuMsg struct{}
+
+type MenuView struct {
+	Name  string
+	Model ViewModel
+	Desc  string
+}
+
+type MenuModel struct {
+	menuItems     []MenuView
+	selectedModel ViewModel
+	help          help.Model
+	menuPos       int
+	db            *database.Database
+}
+
+func NewMenuModel(views []MenuView, db *database.Database) MenuModel {
+	m := MenuModel{}
+	m.db = db
+
+	m.help = help.New()
+	m.help.Styles.ShortKey = ui.HelpKeyStyle
+	m.help.Styles.FullKey = m.help.Styles.ShortKey
+	m.help.Styles.ShortDesc = ui.HelpDescStyle
+	m.help.Styles.FullDesc = m.help.Styles.ShortDesc
+
+	m.menuItems = views
+	return m
+}
+
+func (m MenuModel) Update(msg tea.Msg) (MenuModel, tea.Cmd) {
+	var cmd tea.Cmd
+
+	if m.selectedModel != nil {
+		m.menuItems[m.menuPos].Model, cmd = m.selectedModel.Update(msg)
+		switch msg.(type) {
+		case ExitToMenuMsg:
+			m.selectedModel = nil
+		}
+		return m, cmd
+	}
+
+	itemLen := len(m.menuItems)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		for _, v := range m.menuItems {
+			v.Model, _ = v.Model.Update(msg)
+		}
+
+	case tea.KeyPressMsg:
+		switch {
+		case key.Matches(msg, ui.KeyMap.Exit):
+			return m, exit
+
+		case key.Matches(msg, ui.KeyMap.Select):
+			m.selectedModel = m.menuItems[m.menuPos].Model
+
+		case key.Matches(msg, ui.KeyMap.HelpMore):
+			if m.selectedModel == nil || len(m.selectedModel.FullHelp()) == 0 {
+				break
+			}
+			m.help.ShowAll = !m.help.ShowAll
+
+		case key.Matches(msg, ui.KeyMap.Up):
+			m.menuPos = (m.menuPos - 1 + itemLen) % itemLen
+
+		case key.Matches(msg, ui.KeyMap.Down):
+			m.menuPos = (m.menuPos + 1) % itemLen
+
+		}
+	}
+
+	return m, cmd
+}
+
+func (m MenuModel) View() (string, *tea.Cursor) {
+	if m.selectedModel != nil {
+		v, c := m.selectedModel.View()
+		return lipgloss.JoinVertical(
+			lipgloss.Left,
+			v,
+			ui.HelpStyle.Render(m.help.View(m.selectedModel)),
+		), c
+	}
+
+	return lipgloss.JoinVertical(
+		lipgloss.Left,
+		m.DisplayProfile(),
+		m.DisplayMenu(),
+		ui.HelpStyle.Render(m.help.View(m)),
+	), nil
+}
+
+func (m MenuModel) ShortHelp() []key.Binding {
+	return []key.Binding{
+		ui.KeyMap.Up,
+		ui.KeyMap.Down,
+		ui.KeyMap.Select,
+		ui.KeyMap.Exit,
+	}
+}
+
+func (m MenuModel) FullHelp() [][]key.Binding {
+	if m.selectedModel != nil {
+		return m.menuItems[m.menuPos].Model.FullHelp()
+	}
+	return [][]key.Binding{}
+}
+
+func (m MenuModel) DisplayMenu() string {
+	items := make([]string, len(m.menuItems)+1)
+	menuStyle := ui.TextStyle.MarginLeft(5).Width(12).PaddingLeft(1).PaddingRight(3)
+
+	// Header
+	items[0] = ui.TextStyle.
+		MarginTop(1).
+		MarginBottom(1).
+		Render(utils.ColorText(";b;What would you like to do?"))
+
+	for i, v := range m.menuItems {
+		if m.menuPos == i {
+			items[i+1] = menuStyle.Foreground(ansi.BrightGreen).
+				Background(ansi.Black).
+				Render("> " + v.Name)
+			continue
+		}
+		items[i+1] = menuStyle.Render("  " + v.Name)
+	}
+
+	return lipgloss.JoinVertical(
+		lipgloss.Left,
+		items...,
+	)
+}
+
+func (m MenuModel) DisplayProfile() string {
+	profile := m.db.GetProfile()
+	header := ui.TextStyle.
+		MarginTop(1).
+		MarginBottom(1).
+		Render(utils.ColorText(fmt.Sprintf(";dy;%s's;b; profile stats:", profile.Username)))
+
+	expStyle := ui.Style
+	tokenExpiration := utils.NewRelativeTimeUnits(profile.TokenExpirationSec)
+	switch {
+	case tokenExpiration.Weeks < 1:
+		expStyle = expStyle.Foreground(ansi.BrightRed)
+	case tokenExpiration.Weeks < 2:
+		expStyle = expStyle.Foreground(ansi.BrightYellow)
+	default:
+		expStyle = expStyle.Foreground(ansi.BrightGreen)
+	}
+
+	d := ui.DisplayPropVal([]string{
+		utils.ColorText(";dc;Completed Anime"),
+		utils.ColorText(";dc;Time Watched"),
+		utils.ColorText(";dc;Token Expiration"),
+		utils.ColorText(";dc;Last Updated"),
+	}, []string{
+		strconv.Itoa(profile.CompletedSeries),
+		utils.NewDurationUnits(time.Second * time.Duration(profile.SecondsWatched)).
+			ToShortString(),
+		expStyle.Render(tokenExpiration.ToPrecisionString(utils.Days)),
+		utils.NewRelativeTimeUnits(profile.LastUpdateSec).String(),
+	})
+
+	return lipgloss.JoinVertical(
+		lipgloss.Left,
+		header,
+		d,
+	)
+}
+
+func exitToMenu() tea.Msg {
+	return ExitToMenuMsg{}
+}
--- lib/views/view.go
+++ lib/views/view.go
@@ -2,53 +2,26 @@ package views
 
 import (
 	"fmt"
-	"strconv"
-	"time"
 
 	"github.com/Jaeiya/koshime/lib/database"
 	"github.com/Jaeiya/koshime/lib/ui"
 	"github.com/Jaeiya/koshime/lib/utils"
 	"github.com/charmbracelet/bubbles/v2/help"
-	"github.com/charmbracelet/bubbles/v2/key"
-	"github.com/charmbracelet/bubbles/v2/textinput"
 	tea "github.com/charmbracelet/bubbletea/v2"
 	"github.com/charmbracelet/lipgloss/v2"
-	"github.com/charmbracelet/x/ansi"
 )
 
 type UIView int
 
 const (
-	None = UIView(iota)
-	SetupUser
+	SetupUser = UIView(iota)
 	Menu
-	Exit
-	WatchAnimeView
-	AddAnime
-	DelAnime
-	FindAnime
-	DropAnime
-	RSS
-	Maintenance
 	Abort
-)
-
-type MenuItem int
-
-const (
-	Find = MenuItem(iota)
-	Watch
-	Update
-	Delete
-	Drop
-	Rss
-	Clean
-	Add
+	Exit
 )
 
 type (
-	FetchErrorMsg       error
-	FetchedNoResultsMsg struct{}
+	FetchErrorMsg error
 )
 
 type ViewModel interface {
@@ -57,35 +30,17 @@ type ViewModel interface {
 	Update(msg tea.Msg) (ViewModel, tea.Cmd)
 }
 
-var MenuItemMap = map[MenuItem]string{
-	Find:   "Find",
-	Add:    "Add",
-	Watch:  "Watch",
-	Update: "Update",
-	Delete: "Delete",
-	Drop:   "Drop",
-	Rss:    "RSS",
-	Clean:  "Clean",
-}
-
 type (
-	AbortMsg      struct{}
-	GotoMenuMsg   struct{}
-	WindowSizeMsg struct {
-		Width  int
-		Height int
-	}
+	AbortMsg struct{}
+	ExitMsg  struct{}
 )
 
 type Model struct {
-	db          *database.Database
-	menuItems   []MenuItem
-	menuViewMap map[UIView]ViewModel
-	help        help.Model
-	state       struct {
-		view    UIView
-		menuPos int
-	}
+	db        *database.Database
+	menu      MenuModel
+	help      help.Model
+	setupUser SetupUserModel
+	view      UIView
 }
 
 func New(dbPath string) (Model, error) {
@@ -97,26 +52,18 @@ func New(dbPath string) (Model, error) {
 	m.help.Styles.ShortDesc = ui.HelpDescStyle
 	m.help.Styles.FullDesc = m.help.Styles.ShortDesc
 
-	m.menuItems = append(
-		m.menuItems,
-		Find,
-		Add,
-		Delete,
-	)
-
 	// Initialize empty database
 	db, _ := database.NewDatabase(nil)
 	m.db = db
 
-	m.menuViewMap = map[UIView]ViewModel{
-		SetupUser: newSetupUserModel(),
-		FindAnime: newFindAnimeModel(db),
-		AddAnime:  newAddAnimeModel(db),
-		DelAnime:  newDelAnimeModel(db),
-	}
+	m.menu = NewMenuModel([]MenuView{
+		{Name: "Find", Model: newFindAnimeModel(db), Desc: "Finds an anime anime"},
+		{Name: "Add", Model: newAddAnimeModel(db), Desc: "Adds an anime"},
+		{Name: "Delete", Model: newDelAnimeModel(db), Desc: "Deletes an anime"},
+	}, db)
 
 	if !utils.FileExists(dbPath) {
-		m.SetViewState(SetupUser)
+		m.setupUser = newSetupUserModel()
 		return m, nil
 	}
 
@@ -125,8 +72,7 @@ func New(dbPath string) (Model, error) {
 		return m, err
 	}
 
-	m.SetViewState(Menu)
-
+	m.view = Menu
 	return m, nil
 }
 
@@ -137,227 +83,78 @@ func (ui Model) Init() tea.Cmd {
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	var cmds []tea.Cmd
-	var currentModel ViewModel
-
-	if model, exists := m.menuViewMap[m.state.view]; exists {
-		currentModel = model
-	}
 
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		// Send size to all menu view models
-		for key, model := range m.menuViewMap {
-			m.menuViewMap[key], _ = model.Update(msg)
-		}
+		m.menu, _ = m.menu.Update(msg)
 
 	case tea.KeyPressMsg:
-		switch {
-		case key.Matches(msg, ui.KeyMap.HelpMore):
-			if (currentModel != nil && len(currentModel.FullHelp()) == 0) || m.state.view == Menu {
-				break
-			}
-			m.help.ShowAll = !m.help.ShowAll
-
-		case key.Matches(msg, ui.KeyMap.MainMenu):
-			if m.state.view == Menu {
-				m.state.view = Exit
-				return m, tea.Quit
-			}
-		}
-
 		if msg.String() == "ctrl+c" {
-			m.state.view = Abort
+			m.view = Abort
 			return m, tea.Quit
 		}
 
 	case SetupUserFinishedMsg:
-		m.SetViewState(Menu)
+		m.view = Menu
 		err := m.db.LoadData(msg)
 		if err != nil {
-			// FIX  Do not panic, we should display a message
-			panic(err)
+			// This should never happen
+			panic(fmt.Errorf("failed to load new user data: %w", err))
 		}
 
-	case GotoMenuMsg:
-		m.SetViewState(Menu)
-		return m, nil
-
 	case AbortMsg:
-		m.SetViewState(Abort)
+		m.view = Abort
+		return m, tea.Quit
+
+	case ExitMsg:
+		m.view = Exit
 		return m, tea.Quit
 
 	}
 
-	if currentModel != nil {
-		// We do not want to add new models to the map
-		if _, exists := m.menuViewMap[m.state.view]; exists {
-			m.menuViewMap[m.state.view], cmd = currentModel.Update(msg)
-			cmds = append(cmds, cmd)
-		}
+	if m.view == SetupUser {
+		m.setupUser, cmd = m.setupUser.Update(msg)
+		return m, cmd
 	}
 
-	switch m.state.view {
-	case Menu:
-		m, cmd = m.UpdateMenu(msg)
-		cmds = append(cmds, cmd)
-	}
+	m.menu, cmd = m.menu.Update(msg)
+	cmds = append(cmds, cmd)
 
 	return m, tea.Batch(cmds...)
 }
 
-func (m *Model) SetViewState(view UIView) {
-	m.state.view = view
-}
-
 func (m Model) View() (string, *tea.Cursor) {
-	if model, exists := m.menuViewMap[m.state.view]; exists {
-		view, c := model.View()
+	switch m.view {
+	case Menu:
+		return m.menu.View()
+
+	case SetupUser:
+		v, c := m.setupUser.View()
 		return lipgloss.JoinVertical(
 			lipgloss.Left,
-			view,
-			ui.HelpStyle.Render(m.help.View(model)),
+			v,
+			ui.HelpStyle.Render(m.help.View(m.setupUser)),
 		), c
-	}
 
-	switch m.state.view {
+	case Exit:
+		return "", nil
+
 	case Abort:
 		return ui.AbortStyle.Render(
 			utils.ColorText(";g;>>> ;y;User Aborted Operation ;g;<<<"),
 		), nil
 
-	case Menu:
-		profile := m.db.GetProfile()
-		header := ui.TextStyle.
-			MarginTop(1).
-			MarginBottom(1).
-			Render(utils.ColorText(fmt.Sprintf(";dy;%s's;b; profile stats:", profile.Username)))
+	default:
+		return "missing view", nil
 
-		expStyle := ui.Style
-		tokenExpiration := utils.NewRelativeTimeUnits(profile.TokenExpirationSec)
-		switch {
-		case tokenExpiration.Weeks < 1:
-			expStyle = expStyle.Foreground(ansi.BrightRed)
-		case tokenExpiration.Weeks < 2:
-			expStyle = expStyle.Foreground(ansi.BrightYellow)
-		default:
-			expStyle = expStyle.Foreground(ansi.BrightGreen)
-		}
-
-		d := ui.DisplayPropVal([]string{
-			utils.ColorText(";dc;Completed Anime"),
-			utils.ColorText(";dc;Time Watched"),
-			utils.ColorText(";dc;Token Expiration"),
-			utils.ColorText(";dc;Last Updated"),
-		}, []string{
-			strconv.Itoa(profile.CompletedSeries),
-			utils.NewDurationUnits(time.Second * time.Duration(profile.SecondsWatched)).
-				ToShortString(),
-			expStyle.Render(tokenExpiration.ToPrecisionString(utils.Days)),
-			utils.NewRelativeTimeUnits(profile.LastUpdateSec).String(),
-		})
-
-		return lipgloss.JoinVertical(
-			lipgloss.Left,
-			header,
-			d,
-			m.viewMenu(),
-			ui.TextStyle.Render(m.help.View(m)),
-		), nil
-
-	case Exit:
-		return "", nil
-
-	case None:
-		anime := m.db.GetAllAnime()
-		return ui.Style.PaddingTop(1).PaddingLeft(3).Render(
-			utils.ColorText(fmt.Sprintf(";g;%d ;w;Library Entries Loaded", len(anime))),
-		), nil
 	}
-
-	return "missing view", nil
-}
-
-func (m Model) UpdateMenu(msg tea.Msg) (Model, tea.Cmd) {
-	var cmd tea.Cmd
-	itemLen := len(m.menuItems)
-
-	switch msg := msg.(type) {
-	case tea.KeyPressMsg:
-		switch {
-		case key.Matches(msg, ui.KeyMap.Select):
-			switch m.menuItems[m.state.menuPos] {
-			case Find:
-				m.SetViewState(FindAnime)
-				cmd = textinput.Blink
-			case Add:
-				m.SetViewState(AddAnime)
-				cmd = textinput.Blink
-			case Delete:
-				m.SetViewState(DelAnime)
-				cmd = textinput.Blink
-			}
-
-		case key.Matches(msg, ui.KeyMap.Up):
-			m.state.menuPos = (m.state.menuPos - 1 + itemLen) % itemLen
-
-		case key.Matches(msg, ui.KeyMap.Down):
-			m.state.menuPos = (m.state.menuPos + 1) % itemLen
-
-		}
-	}
-
-	return m, cmd
-}
-
-func (m Model) viewMenu() string {
-	items := make([]string, len(m.menuItems)+1)
-	s := ui.TextStyle.MarginLeft(5).Width(12).PaddingLeft(1).PaddingRight(3)
-
-	header := ui.TextStyle.MarginTop(1).
-		MarginBottom(1).
-		Render(utils.ColorText(";b;What would you like to do?"))
-	items[0] = header
-
-	for i, item := range m.menuItems {
-		if m.state.menuPos == i {
-			items[i+1] = s.Foreground(ansi.BrightGreen).
-				Background(ansi.Black).
-				Render("> " + MenuItemMap[item])
-			continue
-		}
-		items[i+1] = s.Render("  " + MenuItemMap[item])
-	}
-
-	// Add empty line between help list
-	items = append(items, "")
-
-	return lipgloss.JoinVertical(
-		lipgloss.Left,
-		items...,
-	)
-}
-
-func (m Model) ShortHelp() []key.Binding {
-	switch m.state.view {
-	case Menu:
-		return []key.Binding{
-			ui.KeyMap.Up,
-			ui.KeyMap.Down,
-			ui.KeyMap.Select,
-			ui.KeyMap.Exit,
-		}
-	}
-	return []key.Binding{}
-}
-
-func (m Model) FullHelp() [][]key.Binding {
-	return [][]key.Binding{}
 }
 
 func abort() tea.Msg {
 	return AbortMsg{}
 }
 
-func exitToMenu() tea.Msg {
-	return GotoMenuMsg{}
+func exit() tea.Msg {
+	return ExitMsg{}
 }
