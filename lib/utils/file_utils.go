502ca433ad189db12a838f3cfd98224c3128c164
Jaeiya <exanix@gmail.com>
2025-06-02 10:40:19 -0700

    feat(file_utils): add file name streamer/iterator

--- lib/utils/file_utils.go
+++ lib/utils/file_utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -49,4 +50,36 @@ func FileExists(path string) bool {
 		panic(err)
 	}
 	return !info.IsDir()
+}
+
+type FilenameIterator interface {
+	Next() (fileName string, ok bool)
+}
+
+type FilenameStream struct {
+	entries []os.DirEntry
+	index   int
+}
+
+// NewFilenameStream reads the specified directory and returns a
+// struct which allows you to walk through each file name.
+//
+// 🟡 Directory names are ignored
+func NewFilenameStream(dir string) (*FilenameStream, error) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return &FilenameStream{}, fmt.Errorf("could not read dir: %w", err)
+	}
+	return &FilenameStream{entries, 0}, nil
+}
+
+// Next returns the next file name and its status. If there
+// are no more file names, the status is false.
+func (fs *FilenameStream) Next() (string, bool) {
+	if fs.index == len(fs.entries) {
+		return "", false
+	}
+	fileName := fs.entries[fs.index].Name()
+	fs.index++
+	return fileName, true
 }
