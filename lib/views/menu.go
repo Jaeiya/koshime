215d73fbb0fef430df7f193c81fb73155cb18e37
Jaeiya <exanix@gmail.com>
2025-06-18 12:33:40 -0700

    feat(menu): change item description color

--- lib/views/menu.go
+++ lib/views/menu.go
@@ -163,7 +163,7 @@ func (m MenuModel) FullHelp() [][]key.Binding {
 func (m MenuModel) DisplayMenu() string {
 	lines := make([]string, len(m.activeItems))
 	menuStyle := ui.TextStyle.MarginLeft(5).Width(17).PaddingLeft(1).PaddingRight(3)
-	descStyle := ui.Style.Width(30).MarginLeft(3)
+	descStyle := ui.Style.Width(30).MarginLeft(3).Foreground(ansi.Magenta)
 	activeDesc := ""
 
 	for i, item := range m.activeItems {
