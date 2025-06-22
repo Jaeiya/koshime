1232a654f5cc9f2351c23044ae010ddb30d1b8de
Jaeiya <exanix@gmail.com>
2025-06-21 18:12:29 -0700

    feat(kitsu_api): support updating progress

--- lib/kitsu/kitsu_api.go
+++ lib/kitsu/kitsu_api.go
@@ -202,6 +202,36 @@ func DeleteLibAnime(libID, token string) (int, error) {
 	return status, nil
 }
 
+func UpdateProgress(libID, token string, progress int) (int, error) {
+	payload, err := newAnimeProgressPayload(libID, progress)
+	if err != nil {
+		return 0, fmt.Errorf("failed to load progress payload: %w", err)
+	}
+
+	respData := struct {
+		Data struct {
+			Id         string
+			Attributes struct {
+				Progress int
+			}
+		}
+	}{}
+
+	opts := APIReqOptions{
+		method:      apiPatch,
+		url:         getLibEntryURL(libID),
+		contentType: vndAPIContent,
+		payload:     payload,
+		token:       token,
+	}
+	status, err := newAPIRequest(opts, &respData)
+	if err != nil {
+		return 0, err
+	}
+
+	return status, nil
+}
+
 func GetProfile(userName string) (Profile, error) {
 	qurl, err := getProfileQURL(userName)
 	if err != nil {
