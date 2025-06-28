2e1f6077b2f329e66e782742e60059bb6d4ddba6
Jaeiya <exanix@gmail.com>
2025-06-27 19:53:03 -0700

    feat: add view template

---
+++ lib/views/_view_template.go
@@ -0,0 +1,60 @@
+
+import (
+	"github.com/Jaeiya/koshime/lib/ui"
+	"github.com/charmbracelet/bubbles/v2/key"
+	tea "github.com/charmbracelet/bubbletea/v2"
+)
+
+type Some_View int
+
+const (
+	Some_Default = Some_View(iota)
+)
+
+type Some_Model struct {
+	windowSize tea.WindowSizeMsg
+	ui         struct {
+		loader ui.LoaderModel
+	}
+	state Some_State
+}
+
+type Some_State struct {
+	err error
+}
+
+func newSomeModel() Some_Model {
+	m := Some_Model{}
+	return m
+}
+
+func (m Some_Model) Update(msg tea.Msg) (ViewModel, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.windowSize = msg
+
+	case error:
+		m.state.err = msg
+	}
+	return m, nil
+}
+
+func (m Some_Model) View() (string, *tea.Cursor) {
+	if m.ui.loader.IsLoading() {
+		return ui.Style.MarginTop(1).Render(m.ui.loader.View()), nil
+	}
+
+	if m.state.err != nil {
+		return ui.DisplayError(m.state.err), nil
+	}
+
+	return "", nil
+}
+
+func (m Some_Model) ShortHelp() []key.Binding {
+	return []key.Binding{}
+}
+
+func (m Some_Model) FullHelp() [][]key.Binding {
+	return [][]key.Binding{}
+}
