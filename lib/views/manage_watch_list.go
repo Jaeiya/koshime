1643e3781269c10e7510c3269dfe38afe5b69331
Jaeiya <exanix@gmail.com>
2025-06-30 16:31:44 -0700

    feat(watch_list): add menu system & use view states for control flow
    This overhauls the control flow so that we avoid conflicts and have an
    easier way to add more actions.

--- lib/views/manage_watch_list.go
+++ lib/views/manage_watch_list.go
@@ -12,6 +12,16 @@ import (
 	"github.com/charmbracelet/lipgloss/v2"
 )
 
+type WatchList_View int
+
+const (
+	WatchList_Menu = WatchList_View(iota)
+	WatchList_Reload
+	WatchList_Delete
+)
+
+type WatchListReloadedMsg = []kitsu.LibraryEntry
+
 type WatchList_Model struct {
 	windowSize tea.WindowSizeMsg
 	ui         struct {
@@ -21,25 +31,28 @@ type WatchList_Model struct {
 	}
 	keys struct {
 		reload key.Binding
-		delete key.Binding
 	}
-	db    *database.Database
-	state WatchList_State
+	db        *database.Database
+	menuItems []string
+	state     WatchList_State
 }
 
 type WatchList_State struct {
-	err         error
-	anime       []ui.AnimeInfo
-	animeIndex  int
-	isReloading bool
+	err        error
+	view       WatchList_View
+	anime      []ui.AnimeInfo
+	animeIndex int
+	menuIndex  int
 }
 
 func newWatchListModel(db *database.Database) WatchList_Model {
 	m := WatchList_Model{db: db}
+	m.menuItems = []string{
+		"Delete",
+	}
 	m.ui.loader = ui.NewLoader()
 	m.ui.animeDisplay = NewAnimeDisplayModel()
 	m.keys.reload = key.NewBinding(key.WithKeys("r"), key.WithHelp("r", "reload"))
-	m.keys.delete = key.NewBinding(key.WithKeys("d"), key.WithHelp("d", "delete"))
 	m.state.anime = ui.ToAnimeInfo(db.Anime())
 	return m
 }
@@ -50,9 +63,7 @@ func (m WatchList_Model) Init() tea.Cmd {
 
 func (m WatchList_Model) Update(msg tea.Msg) (ViewModel, tea.Cmd) {
 	var cmd tea.Cmd
-	if m.state.isReloading {
-		m.ui.consent = m.ui.consent.Update(msg)
-	}
+	var cmds []tea.Cmd
 
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
@@ -61,37 +72,16 @@ func (m WatchList_Model) Update(msg tea.Msg) (ViewModel, tea.Cmd) {
 	case tea.KeyPressMsg:
 		switch {
 		case key.Matches(msg, m.keys.reload):
-			m.state.isReloading = true
+			// Never execute reload in another view
+			if m.state.view > WatchList_Menu {
+				break
+			}
+			m.state.view = WatchList_Reload
 
 		case key.Matches(msg, ui.KeyMap.MainMenu):
 			return m, exitToMenu
 
-		case key.Matches(msg, ui.KeyMap.Select):
-			if m.state.isReloading {
-				m.state = WatchList_State{}
-				if m.ui.consent.Select() == ui.No {
-					return m, nil
-				}
-				m.ui.loader, cmd = m.ui.loader.Start("Reloading Watch List")
-				return m, tea.Batch(cmd, m.reloadLibrary)
-			}
-
-		case key.Matches(msg, ui.KeyMap.Left):
-			if m.state.animeIndex == 0 {
-				return m, nil
-			}
-			m.state.animeIndex--
-
-		case key.Matches(msg, ui.KeyMap.Right):
-			if m.state.animeIndex == len(m.state.anime)-1 {
-				return m, nil
-			}
-			m.state.animeIndex++
 		}
-
-	case []kitsu.LibraryEntry:
-		m.state.anime = ui.ToAnimeInfo(msg)
-		m.ui.loader.Stop()
 
 	case error:
 		m.state.err = msg
@@ -99,9 +89,24 @@ func (m WatchList_Model) Update(msg tea.Msg) (ViewModel, tea.Cmd) {
 
 	if m.ui.loader.IsLoading() {
 		m.ui.loader, cmd = m.ui.loader.Update(msg)
+		cmds = append(cmds, cmd)
 	}
 
-	return m, cmd
+	switch m.state.view {
+	case WatchList_Menu:
+		m, cmd = m.UpdateMenu(msg)
+		cmds = append(cmds, cmd)
+
+	case WatchList_Reload:
+		m, cmd = m.UpdateReload(msg)
+		cmds = append(cmds, cmd)
+
+	case WatchList_Delete:
+		m, cmd = m.UpdateDelete(msg)
+		cmds = append(cmds, cmd)
+	}
+
+	return m, tea.Batch(cmds...)
 }
 
 func (m WatchList_Model) View() (string, *tea.Cursor) {
@@ -113,8 +118,11 @@ func (m WatchList_Model) View() (string, *tea.Cursor) {
 		return ui.DisplayError(m.state.err), nil
 	}
 
-	if m.state.isReloading {
-		return m.ViewReloading(), nil
+	switch m.state.view {
+	case WatchList_Reload:
+		return m.ViewReload(), nil
+	case WatchList_Delete:
+		return m.ViewDeleting(), nil
 	}
 
 	view := lipgloss.JoinVertical(
@@ -126,11 +134,79 @@ func (m WatchList_Model) View() (string, *tea.Cursor) {
 		}),
 		"",
 		m.ui.animeDisplay.View(m.state.anime[m.state.animeIndex]),
+		ui.DisplayTitle("Manage"),
+		"",
+		ui.DisplayText([]string{
+			`Select an action to apply to the above entry.`,
+		}),
+		"",
+		ui.DisplayMenuItems(m.menuItems, 0),
 	)
 	return view, nil
 }
 
-func (m WatchList_Model) ViewReloading() string {
+func (m WatchList_Model) UpdateMenu(msg tea.Msg) (WatchList_Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch {
+		case key.Matches(msg, ui.KeyMap.Left):
+			if m.state.animeIndex == 0 {
+				break
+			}
+			m.state.animeIndex--
+
+		case key.Matches(msg, ui.KeyMap.Right):
+			if m.state.animeIndex == len(m.state.anime)-1 {
+				break
+			}
+			m.state.animeIndex++
+
+		case key.Matches(msg, ui.KeyMap.Up):
+			if m.state.menuIndex == 0 {
+				break
+			}
+			m.state.menuIndex--
+
+		case key.Matches(msg, ui.KeyMap.Down):
+			if m.state.menuIndex == len(m.menuItems)-1 {
+				break
+			}
+			m.state.menuIndex++
+
+		case key.Matches(msg, ui.KeyMap.Select):
+			if m.state.menuIndex == 0 {
+				m.state.view = WatchList_Delete
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m WatchList_Model) UpdateReload(msg tea.Msg) (WatchList_Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch {
+		case key.Matches(msg, ui.KeyMap.Select):
+			if m.ui.consent.Select() == ui.No {
+				m.state.view = WatchList_Menu
+				return m, nil
+			}
+			m.ui.loader, cmd = m.ui.loader.Start("Reloading Watch List")
+			return m, tea.Batch(cmd, m.reloadLibrary)
+		}
+
+	case WatchListReloadedMsg:
+		m.state = WatchList_State{}
+		m.state.anime = ui.ToAnimeInfo(msg)
+		m.ui.loader.Stop()
+
+	}
+	m.ui.consent = m.ui.consent.Update(msg)
+	return m, nil
+}
+
+func (m WatchList_Model) ViewReload() string {
 	view := lipgloss.JoinVertical(
 		lipgloss.Left,
 		ui.DisplaySubTitle("Watch List", "Reloading"),
@@ -152,12 +228,56 @@ manually update your Kitsu watch list from the website.`,
 	return view
 }
 
+func (m WatchList_Model) UpdateDelete(msg tea.Msg) (WatchList_Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch {
+		case key.Matches(msg, ui.KeyMap.Select):
+			if m.ui.consent.Select() == ui.No {
+				m.state.view = WatchList_Menu
+				return m, nil
+			}
+			m.ui.loader, cmd = m.ui.loader.Start("Deleting Entry")
+			return m, tea.Batch(cmd, m.deleteEntry)
+		}
+
+	case WatchListReloadedMsg:
+		m.state = WatchList_State{}
+		m.state.anime = ui.ToAnimeInfo(msg)
+		m.ui.loader.Stop()
+	}
+
+	m.ui.consent = m.ui.consent.Update(msg)
+	return m, nil
+}
+
+func (m WatchList_Model) ViewDeleting() string {
+	view := lipgloss.JoinVertical(
+		lipgloss.Left,
+		ui.DisplaySubTitle("Watch List", "Deleting Entry"),
+		"",
+		ui.DisplayText(
+			[]string{
+				fmt.Sprintf(
+					`;dc;%s ;x;will be deleted from your Kitsu library and
+the local database.`, m.state.anime[m.state.animeIndex].EngTitle),
+				`;y;[this action cannot be undone]`,
+			}, 1,
+		),
+		ui.TextStyle.Render(
+			m.ui.consent.View(utils.ColorText(";b;Are you sure?")),
+		),
+	)
+	return view
+}
+
 func (m WatchList_Model) ShortHelp() []key.Binding {
-	if m.state.isReloading {
+	if m.state.view == WatchList_Reload {
 		return []key.Binding{ui.KeyMap.Up, ui.KeyMap.Down, ui.KeyMap.Select, ui.KeyMap.EscBack}
 	}
 
-	keys := []key.Binding{m.keys.reload, m.keys.delete}
+	keys := []key.Binding{ui.KeyMap.Up, ui.KeyMap.Down}
 
 	if len(m.state.anime) > 0 {
 		switch {
@@ -171,13 +291,15 @@ func (m WatchList_Model) ShortHelp() []key.Binding {
 			keys = append(keys, ui.KeyMap.Next)
 		}
 	}
-
-	keys = append(keys, ui.KeyMap.MainMenu)
+	keys = append(keys, ui.KeyMap.HelpMore)
 	return keys
 }
 
 func (m WatchList_Model) FullHelp() [][]key.Binding {
-	return [][]key.Binding{}
+	return [][]key.Binding{
+		{ui.KeyMap.Up, ui.KeyMap.Down, ui.KeyMap.Prev, ui.KeyMap.Next, ui.KeyMap.Select},
+		{m.keys.reload, ui.KeyMap.MainMenu, ui.KeyMap.HelpLess},
+	}
 }
 
 func (m WatchList_Model) reloadLibrary() tea.Msg {
@@ -193,4 +315,19 @@ func (m WatchList_Model) reloadLibrary() tea.Msg {
 	}
 
 	return watchList
+}
+
+func (m WatchList_Model) deleteEntry() tea.Msg {
+	p := m.db.Profile()
+	libID := m.state.anime[m.state.animeIndex].LibID
+	_, err := kitsu.DeleteLibAnime(libID, p.AccessToken)
+	if err != nil {
+		return err
+	}
+	err = m.db.DeleteAnimeById(libID)
+	if err != nil {
+		return fmt.Errorf("failed to delete anime from database: %w", err)
+	}
+
+	return m.db.Anime()
 }
