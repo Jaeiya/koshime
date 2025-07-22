49cdb644f137ba961cfd327bd9afa05ff9e6d398
Jaeiya <exanix@gmail.com>
2025-07-22 13:39:19 -0700

    docs(main): leave commented test code for future use

--- main.go
+++ main.go
@@ -24,5 +24,29 @@ func main() {
 		panic(err)
 	}
 
+	// db, err := database.NewDatabase(nil)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// db.Load()
+
+	// var ff lib.FansubFilter
+	// var fileSys utils.FileSys
+
+	// stream, err := fileSys.NewFilenameStream(fileSys.GetWorkingDir())
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// f, err := ff.FilterByLibEntry(stream, db.Anime())
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for _, file := range f {
+	// 	fmt.Println(file.LibEntry.JPN_Title, file.Score)
+	// 	fmt.Println(file.FileInfo.Filename)
+	// }
+
 	// 	fmt.Println("    Date:", entry.Date.Format("01/02/2006 3:04 PM"))
 }
