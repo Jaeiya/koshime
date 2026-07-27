23f6fc7d6f737a9ba5e9f7b1cd6009e1b91ebae8
Jaeiya <exanix@gmail.com>
2026-07-27 13:04:29 -0700

    fix(qbt): forgot to save rule after feed removal

--- lib/qbittorrent/qbt_api.go
+++ lib/qbittorrent/qbt_api.go
@@ -145,6 +145,7 @@ func (qb qBittorrentAPI) AddRuleFeed(name, feed string) error {
 	if err != nil {
 		return fmt.Errorf("failed to add rule feed: %w", err)
 	}
+
 	ruleCache[name] = rule
 	return nil
 }
@@ -156,6 +157,12 @@ func (qb qBittorrentAPI) DeleteRuleFeed(name, feed string) error {
 	}
 
 	rule.RemoveFeed(feed)
+	err := qb.saveRule(name, rule, ModifyRule)
+	if err != nil {
+		return fmt.Errorf("failed to add rule feed: %w", err)
+	}
+
+	ruleCache[name] = rule
 	return nil
 }
 
