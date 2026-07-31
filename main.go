84f7a8508514aa83592fee3c3879f75078b80524
Jaeiya <exanix@gmail.com>
2026-07-31 15:46:17 -0700

    chore(main): remove test code

--- main.go
+++ main.go
@@ -23,30 +23,4 @@ func main() {
 	if _, err := p.Run(); err != nil {
 		panic(err)
 	}
-
-	// db, err := database.NewDatabase(nil)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// db.Load()
-
-	// var ff lib.FansubFilter
-	// var fileSys utils.FileSys
-
-	// stream, err := fileSys.NewFilenameStream(fileSys.GetWorkingDir())
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// f, err := ff.FilterByLibEntry(stream, db)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// for _, file := range f {
-	// 	fmt.Println(file.LibEntry.JPN_Title, file.Score)
-	// 	fmt.Printf("%s\n\n", file.FileInfo.Filename)
-	// }
-
-	// 	fmt.Println("    Date:", entry.Date.Format("01/02/2006 3:04 PM"))
 }
