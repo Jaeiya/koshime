ced9736968a59585e8e911c4a236f90b92d878f5
Jaeiya <exanix@gmail.com>
2026-08-05 17:34:48 -0700

    feat(views_anime_display): only display `1` alt title in simple mode
    Refactored the anime display func into the anime display view file.

--- lib/ui/ui_helpers.go
+++ lib/ui/ui_helpers.go
@@ -2,38 +2,13 @@ package ui
 
 import (
 	"fmt"
-	"net/url"
-	"os"
 	"strings"
 
 	"github.com/Jaeiya/koshime/lib/kitsu"
 	"github.com/Jaeiya/koshime/lib/utils"
 	"github.com/charmbracelet/lipgloss/v2"
 	"github.com/charmbracelet/x/ansi"
-	"github.com/charmbracelet/x/term"
 )
-
-type DisplayMode uint8
-
-const (
-	Simple = DisplayMode(iota)
-	Extended
-	All
-)
-
-var MaxSynopsisLen = func() int {
-	_, h, err := term.GetSize(os.Stdout.Fd())
-	if err != nil {
-		return 500
-	}
-
-	const menuSize = 23.0 // Estimation of menu size
-	const padding = 0.20  // Prevent filling entire screen
-	const charsPerLine = 40
-
-	ratio := (1.0 - (menuSize / float64(h))) - padding
-	return int((float64(h) * ratio)) * charsPerLine
-}()
 
 type AnimeInfo struct {
 	ID        string
@@ -71,124 +46,6 @@ func DisplayCharLimit(min int, text string) string {
 	}
 
 	return charLimit
-}
-
-func DisplayAnimeInfo(info AnimeInfo, mode DisplayMode) string {
-	headers := []string{
-		utils.ColorText(";g;Title"),
-		utils.ColorText(";dc;English"),
-	}
-
-	items := []string{
-		info.JpnTitle,
-		info.EngTitle,
-	}
-
-	if items[1] == "" {
-		items[1] = utils.ColorText(";bk;None")
-	}
-
-	if len(info.AltTitles) > 0 {
-		for i := range len(info.AltTitles) {
-			if utils.HasNonASCII(info.AltTitles[i]) {
-				continue
-			}
-			headers = append(headers, utils.ColorText(";db;AltTitle"))
-		}
-	} else {
-		headers = append(headers, utils.ColorText(";db;AltTitle"))
-	}
-
-	if Simple == mode {
-		for i, item := range items {
-			if len(item) > 40 {
-				items[i] = item[:37] + "..."
-			}
-		}
-		if len(info.AltTitles) > 0 {
-			for _, title := range info.AltTitles {
-				if utils.HasNonASCII(title) {
-					continue
-				}
-				if len(title) > 40 {
-					title = title[:37] + "..."
-				}
-				items = append(items, title)
-			}
-		} else {
-			items = append(items, utils.ColorText(";bk;None"))
-		}
-	}
-
-	if Extended == mode || All == mode {
-		if len(info.AltTitles) > 0 {
-			for _, title := range info.AltTitles {
-				if utils.HasNonASCII(title) {
-					continue
-				}
-				items = append(items, title)
-			}
-		} else {
-			items = append(items, utils.ColorText(";bk;None"))
-		}
-	}
-
-	headers = append(headers, utils.ColorText(";dc;Status"))
-	items = append(items, utils.ColorText(";b;"+info.Status))
-
-	headers = append(headers, utils.ColorText(";y;Type"))
-	items = append(items, utils.ColorText(";c;"+info.ShowType))
-
-	totalEpsStr := utils.ColorText(";bk;Unknown")
-	if info.Episodes > 0 {
-		totalEpsStr = fmt.Sprintf(";m;%d", info.Episodes)
-	}
-
-	if info.Progress > -1 {
-		headers = append(headers, utils.ColorText(";y;Progress"))
-		items = append(items, utils.ColorText(
-			fmt.Sprintf(";dg;%d ;y;/ %s", info.Progress, totalEpsStr),
-		))
-	} else {
-		headers = append(headers, utils.ColorText(";dc;Episodes"))
-		items = append(items, utils.ColorText(fmt.Sprintf(";m;%s", totalEpsStr)))
-	}
-
-	avgRating := utils.ColorText(fmt.Sprintf(";w;%s", info.AvgRating))
-	if info.AvgRating == "" {
-		avgRating = utils.ColorText(";bk;Not Calculated")
-	}
-	headers = append(headers, utils.ColorText(";dc;AvgRating"))
-	items = append(items, avgRating)
-
-	hasFeed := info.QbtFeed.Name != ""
-
-	headers = append(headers, utils.ColorText(";dc;RSSFeed"))
-	if hasFeed {
-		items = append(items, utils.ColorText(";g;Yes"))
-	} else {
-		items = append(items, utils.ColorText(";m;No"))
-	}
-
-	link, _ := url.JoinPath(kitsu.KitsuDomain, "anime", info.Slug)
-
-	if All == mode {
-		headers = append(
-			headers,
-			utils.ColorText(";x;Link"),
-			utils.ColorText(";dc;Synopsis"),
-		)
-		if len(info.Synopsis) > MaxSynopsisLen {
-			info.Synopsis = info.Synopsis[:MaxSynopsisLen-3] + "..."
-		}
-		items = append(
-			items,
-			utils.ColorText(";dy;"+link),
-			info.Synopsis,
-		)
-	}
-
-	return DisplayPropValue(headers, items)
 }
 
 // DisplayPropVal converts a slice of properties and values
--- lib/views/anime_display.go
+++ lib/views/anime_display.go
@@ -1,16 +1,45 @@
 package views
 
 import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/Jaeiya/koshime/lib/kitsu"
 	"github.com/Jaeiya/koshime/lib/ui"
+	"github.com/Jaeiya/koshime/lib/utils"
 	"github.com/charmbracelet/bubbles/v2/key"
 	tea "github.com/charmbracelet/bubbletea/v2"
+	"github.com/charmbracelet/x/term"
 )
+
+type DisplayMode uint8
+
+const (
+	Simple = DisplayMode(iota)
+	Extended
+	All
+)
+
+var MaxSynopsisLen = func() int {
+	_, h, err := term.GetSize(os.Stdout.Fd())
+	if err != nil {
+		return 500
+	}
+
+	const menuSize = 23.0 // Estimation of menu size
+	const padding = 0.20  // Prevent filling entire screen
+	const charsPerLine = 40
+
+	ratio := (1.0 - (menuSize / float64(h))) - padding
+	return int((float64(h) * ratio)) * charsPerLine
+}()
 
 type AnimeDisplayModel struct {
 	keys struct {
 		displayMode key.Binding
 	}
-	displayMode ui.DisplayMode
+	displayMode DisplayMode
 }
 
 func NewAnimeDisplayModel() *AnimeDisplayModel {
@@ -25,21 +54,146 @@ func (m *AnimeDisplayModel) Update(msg tea.Msg) {
 		switch {
 		case key.Matches(msg, m.keys.displayMode):
 			switch m.displayMode {
-			case ui.Simple:
-				m.displayMode = ui.Extended
-			case ui.Extended:
-				m.displayMode = ui.All
-			case ui.All:
-				m.displayMode = ui.Simple
+			case Simple:
+				m.displayMode = Extended
+			case Extended:
+				m.displayMode = All
+			case All:
+				m.displayMode = Simple
 			}
 		}
 	}
 }
 
 func (m AnimeDisplayModel) View(ai ui.AnimeInfo) string {
-	return ui.DisplayAnimeInfo(ai, m.displayMode)
+	return m.DisplayAnimeInfo(ai, m.displayMode)
 }
 
 func (m AnimeDisplayModel) ShortHelp() []key.Binding {
 	return []key.Binding{m.keys.displayMode}
+}
+
+func (m AnimeDisplayModel) DisplayAnimeInfo(info ui.AnimeInfo, mode DisplayMode) string {
+	headers := []string{
+		utils.ColorText(";g;Title"),
+		utils.ColorText(";dc;English"),
+	}
+
+	items := []string{
+		info.JpnTitle,
+		info.EngTitle,
+	}
+
+	if items[1] == "" {
+		items[1] = utils.ColorText(";bk;None")
+	}
+
+	// Accept only utf8 titles
+	altTitles := make([]string, 0, len(info.AltTitles))
+	for i := range len(info.AltTitles) {
+		if utils.HasNonASCII(info.AltTitles[i]) {
+			continue
+		}
+		altTitles = append(altTitles, info.AltTitles[i])
+	}
+
+	if len(altTitles) > 0 {
+		for range len(altTitles) {
+			headers = append(headers, utils.ColorText(";db;AltTitle"))
+			if Simple == mode {
+				break
+			}
+		}
+	} else {
+		headers = append(headers, utils.ColorText(";db;AltTitle"))
+	}
+
+	const charLimit = 60
+
+	if Simple == mode {
+		for i, item := range items {
+			if len(item) > charLimit {
+				items[i] = item[:charLimit-2] + ".."
+			}
+		}
+		if len(altTitles) > 0 {
+			title := altTitles[0]
+			if len(title) > charLimit {
+				title = title[:charLimit-2] + ".."
+			}
+			items = append(items, title)
+		} else {
+			items = append(items, utils.ColorText(";bk;None"))
+		}
+	}
+
+	if Extended == mode || All == mode {
+		if len(info.AltTitles) > 0 {
+			for _, title := range info.AltTitles {
+				if utils.HasNonASCII(title) {
+					continue
+				}
+				items = append(items, title)
+			}
+		} else {
+			items = append(items, utils.ColorText(";bk;None"))
+		}
+	}
+
+	headers = append(headers, utils.ColorText(";dc;Status"))
+	items = append(items, utils.ColorText(";b;"+info.Status))
+
+	headers = append(headers, utils.ColorText(";y;Type"))
+	items = append(items, utils.ColorText(";c;"+info.ShowType))
+
+	totalEpsStr := utils.ColorText(";bk;Unknown")
+	if info.Episodes > 0 {
+		totalEpsStr = fmt.Sprintf(";m;%d", info.Episodes)
+	}
+
+	if info.Progress > -1 {
+		headers = append(headers, utils.ColorText(";y;Progress"))
+		items = append(items, utils.ColorText(
+			fmt.Sprintf(";dg;%d ;y;/ %s", info.Progress, totalEpsStr),
+		))
+	} else {
+		headers = append(headers, utils.ColorText(";dc;Episodes"))
+		items = append(items, utils.ColorText(fmt.Sprintf(";m;%s", totalEpsStr)))
+	}
+
+	avgRating := utils.ColorText(fmt.Sprintf(";w;%s", info.AvgRating))
+	if info.AvgRating == "" {
+		avgRating = utils.ColorText(";bk;Not Calculated")
+	}
+	headers = append(headers, utils.ColorText(";dc;AvgRating"))
+	items = append(items, avgRating)
+
+	hasFeed := info.QbtFeed.Name != ""
+
+	headers = append(headers, utils.ColorText(";dc;RSSFeed"))
+	if hasFeed {
+		items = append(items, utils.ColorText(";g;Yes"))
+	} else {
+		items = append(items, utils.ColorText(";m;No"))
+	}
+
+	link, _ := url.JoinPath(kitsu.KitsuDomain, "anime", info.Slug)
+
+	if All == mode {
+		headers = append(
+			headers,
+			utils.ColorText(";x;Link"),
+			utils.ColorText(";dc;Synopsis"),
+		)
+		if len(info.Synopsis) > MaxSynopsisLen {
+			info.Synopsis = info.Synopsis[:MaxSynopsisLen-3] + "..."
+		}
+		items = append(
+			items,
+			utils.ColorText(";dy;"+link),
+			info.Synopsis,
+		)
+	}
+
+	return ui.DisplayPropValue(headers, items)
 }
