5f85740c558fd822ea32ce990417bc5f5b35a19d
Jaeiya <exanix@gmail.com>
2026-08-15 17:30:43 -0700

    test(fansub_filter): add testing to `FilterByLibEntry2()`

--- internal/app/fansub_filter_test.go
+++ internal/app/fansub_filter_test.go
@@ -9,6 +9,279 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestFansubFilterByLibEntry(t *testing.T) {
+	ff := FansubFilter{}
+	fs := utils.FileSys{}
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		stream   utils.FilenameIterator
+		actual   []kitsu.Anime
+		expected []FilteredAnime
+	}{
+		{
+			name:     "not error with empty input",
+			stream:   fs.GenFilenameStream("some file name.mkv"),
+			actual:   []kitsu.Anime{},
+			expected: nil,
+		},
+		{
+			name:   "return nil when no results found",
+			stream: fs.GenFilenameStream("[group] some title.mkv"),
+			actual: []kitsu.Anime{
+				{
+					JPN_Title: "the thing",
+				},
+			},
+			expected: nil,
+		},
+		{
+			name:   "not match lowest threshold anime title match",
+			stream: fs.GenFilenameStream("[group] some title.mkv"),
+			actual: []kitsu.Anime{
+				{
+					JPN_Title: "title",
+				},
+			},
+			expected: nil,
+		},
+		{
+			name:   "not match lowest threshold stream title match",
+			stream: fs.GenFilenameStream("[group] title.mkv"),
+			actual: []kitsu.Anime{
+				{
+					JPN_Title: "some title",
+				},
+			},
+			expected: nil,
+		},
+		{
+			name:   "do not match across title boundaries",
+			stream: fs.GenFilenameStream("[group] this is not a match.mkv"),
+			actual: []kitsu.Anime{
+				{
+					JPN_Title: "this",
+					ENG_Title: "is not",
+					AltTitles: []string{
+						"a", "match",
+					},
+				},
+			},
+			expected: nil,
+		},
+		{
+			name:   "match exactly",
+			stream: fs.GenFilenameStream("[group] is a match.mkv"),
+			actual: []kitsu.Anime{
+				{
+					JPN_Title: "is a match",
+				},
+			},
+			expected: []FilteredAnime{
+				{
+					Anime: kitsu.Anime{JPN_Title: "is a match"},
+					FileInfo: FansubFileInfo{
+						Fansub:   "group",
+						Title:    "is a match",
+						Filename: "[group] is a match.mkv",
+					},
+					Score: 100,
+				},
+			},
+		},
+		{
+			name:   "match exactly via fuzzy matching stream title",
+			stream: fs.GenFilenameStream("[group] match is a.mkv"),
+			actual: []kitsu.Anime{
+				{
+					JPN_Title: "is a match",
+				},
+			},
+			expected: []FilteredAnime{
+				{
+					Anime: kitsu.Anime{JPN_Title: "is a match"},
+					FileInfo: FansubFileInfo{
+						Fansub:   "group",
+						Title:    "match is a",
+						Filename: "[group] match is a.mkv",
+					},
+					Score: 100,
+				},
+			},
+		},
+		{
+			name:   "match exactly via fuzzy matching anime title",
+			stream: fs.GenFilenameStream("[group] is a match.mkv"),
+			actual: []kitsu.Anime{
+				{
+					JPN_Title: "match is a",
+				},
+			},
+			expected: []FilteredAnime{
+				{
+					Anime: kitsu.Anime{JPN_Title: "match is a"},
+					FileInfo: FansubFileInfo{
+						Fansub:   "group",
+						Title:    "is a match",
+						Filename: "[group] is a match.mkv",
+					},
+					Score: 100,
+				},
+			},
+		},
+		{
+			name:   "match partial anime title",
+			stream: fs.GenFilenameStream("[group] a partial match.mkv"),
+			actual: []kitsu.Anime{
+				{
+					JPN_Title: "partial match",
+				},
+			},
+			expected: []FilteredAnime{
+				{
+					Anime: kitsu.Anime{JPN_Title: "partial match"},
+					FileInfo: FansubFileInfo{
+						Fansub:   "group",
+						Title:    "a partial match",
+						Filename: "[group] a partial match.mkv",
+					},
+					Score: 66,
+				},
+			},
+		},
+		{
+			name:   "match partial stream title",
+			stream: fs.GenFilenameStream("[group] partial match.mkv"),
+			actual: []kitsu.Anime{
+				{
+					JPN_Title: "a partial match",
+				},
+			},
+			expected: []FilteredAnime{
+				{
+					Anime: kitsu.Anime{JPN_Title: "a partial match"},
+					FileInfo: FansubFileInfo{
+						Fansub:   "group",
+						Title:    "partial match",
+						Filename: "[group] partial match.mkv",
+					},
+					Score: 66,
+				},
+			},
+		},
+		{
+			name: "match on unique fansub titles only",
+			stream: fs.GenFilenameStream(
+				"[group] file name - 01.mkv",
+				"[group] file name - 02.mkv",
+				"[group] file name - 03.mkv",
+				"[group] file name - 04.mkv",
+				"[group] another file name - 01.mkv",
+				"[group] another file name - 02.mkv",
+				"[group] another file name - 03.mkv",
+			),
+			actual: []kitsu.Anime{
+				{ID: "0", JPN_Title: "file name"},
+				{ID: "1", JPN_Title: "another file name"},
+			},
+			expected: []FilteredAnime{
+				{
+					Anime: kitsu.Anime{ID: "0", JPN_Title: "file name"},
+					FileInfo: FansubFileInfo{
+						Fansub:   "group",
+						Title:    "file name",
+						Episode:  "01",
+						Filename: "[group] file name - 01.mkv",
+					},
+					Score: 100,
+				},
+				{
+					Anime: kitsu.Anime{ID: "1", JPN_Title: "another file name"},
+					FileInfo: FansubFileInfo{
+						Fansub:   "group",
+						Title:    "another file name",
+						Episode:  "01",
+						Filename: "[group] another file name - 01.mkv",
+					},
+					Score: 100,
+				},
+			},
+		},
+		{
+			name: "match only top scoring partial anime title match",
+			stream: fs.GenFilenameStream(
+				"[group] a b c d e f g h i j k l m n o p q r s t u v w x y z.mkv",
+			),
+			actual: []kitsu.Anime{
+				{ // 61% match
+					ID:        "0",
+					JPN_Title: "a b c d e f g h i j k l m n o p",
+				},
+				{ // 53% match
+					ID:        "1",
+					JPN_Title: "a b c d e f g h i j k l m n",
+				},
+				{ // 84% match
+					ID:        "2",
+					JPN_Title: "a b c d e f g h i j k l m n o p q r s t u v",
+				},
+				{ // 73% match
+					ID:        "3",
+					JPN_Title: "a b c d e f g h i j k l m n o p q r s",
+				},
+			},
+			expected: []FilteredAnime{
+				{
+					Anime: kitsu.Anime{
+						ID:        "2",
+						JPN_Title: "a b c d e f g h i j k l m n o p q r s t u v",
+					},
+					FileInfo: FansubFileInfo{
+						Fansub:   "group",
+						Title:    "a b c d e f g h i j k l m n o p q r s t u v w x y z",
+						Filename: "[group] a b c d e f g h i j k l m n o p q r s t u v w x y z.mkv",
+					},
+					Score: 84,
+				},
+			},
+		},
+		{
+			name: "match only top scoring partial stream title match",
+			stream: fs.GenFilenameStream(
+				"[group] a b c d e f g h i j k l m n o p.mkv",             // 61% match
+				"[group] a b c d e f g h i j k l m n.mkv",                 // 53% match
+				"[group] a b c d e f g h i j k l m n o p q r s t u v.mkv", // 84% match
+				"[group] a b c d e f g h i j k l m n o p q r s.mkv",       // 73% match
+			),
+			actual: []kitsu.Anime{
+				{JPN_Title: "a b c d e f g h i j k l m n o p q r s t u v w x y z"},
+			},
+			expected: []FilteredAnime{
+				{
+					Anime: kitsu.Anime{
+						JPN_Title: "a b c d e f g h i j k l m n o p q r s t u v w x y z",
+					},
+					FileInfo: FansubFileInfo{
+						Fansub:   "group",
+						Title:    "a b c d e f g h i j k l m n o p q r s t u v",
+						Filename: "[group] a b c d e f g h i j k l m n o p q r s t u v.mkv",
+					},
+					Score: 84,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run("should "+tt.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := ff.FilterByLibEntry2(tt.stream, tt.actual)
+			require.NoError(t, err)
+			assert.Equal(t, tt.expected, got)
+		})
+	}
+}
+
 func TestScore(t *testing.T) {
 	ff := FansubFilter{}
 	t.Parallel()
@@ -68,6 +341,12 @@ func TestScore(t *testing.T) {
 			title:     "Academia Hero My",
 			anime:     kitsu.Anime{ENG_Title: "My Hero Academia"},
 			wantScore: 100,
+		},
+		{
+			desc:      "match when anime word map is less than title length",
+			title:     "My Hero Academia Extra Words",
+			anime:     kitsu.Anime{ENG_Title: "My Hero Academia"},
+			wantScore: 60,
 		},
 	}
 
